764cc04d9375b33ddea0f140a4ce100f9351d1fc
Walker Eacho <walker.eacho@gmail.com>
2017-07-23 15:12:07 -0400

    Redesigned header/content interaction; first content

--- js/code_dwe.js
+++ js/code_dwe.js
@@ -1,21 +1,33 @@
 var $myWindow = $(window);
 var $myHeader = $("header");
-
+var headerFlag = false;
 $myWindow.on('scroll', function() {
 
-    if($myWindow.scrollTop()>75) {
-        if(!$myHeader.hasClass("smaller")) {
-            $myHeader.addClass("smaller");
-            $myHeader.removeAttr("style");
-        }
-    }else{
-        if($myHeader.hasClass("smaller")) {
+    if(!headerFlag && $myHeader.hasClass("smaller") && $myWindow.scrollTop()<275) {
             $myHeader.removeClass("smaller");
             $myHeader.height($myWindow.height());
-        }
     }
-
 });
+
+$("#exp_link").click(function() {
+    $myWindow.scrollTop(0);
+    removeHeader();
+    $('html,body').animate({
+        scrollTop: $("#exp_header").offset().top},
+        950, 
+        "swing",
+        function() {
+            headerFlag=false;
+        });
+});
+
+function removeHeader() {
+    if(!$myHeader.hasClass("smaller")) {
+        $myHeader.addClass("smaller");
+        $myHeader.removeAttr("style");
+        headerFlag = true;
+    }
+}
 
 function init() {
     $myHeader.height($myWindow.height());
